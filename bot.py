1564933103613fa3e42818a6cd4e072ac908c96b
Charles DiPaolo <realluigiman360@gmail.com>
2021-03-28 18:00:54 -0400

    Nice.

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import discord
 import yaml
 
+config = yaml.safe_load(open("config.yml", "r"))
+
 class Discrod(discord.Client):
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        if message.content.startswith(config['prefix']):
+            args = message.content[len(config["prefix"]):].split(' ')
+            print(args)
+        elif "69" in message.content:
+            await message.channel.send("Nice.", reference = message)
 
 client = Discrod()
-config = yaml.safe_load(open("config.yml", "r"))
 client.run(config["token"])
